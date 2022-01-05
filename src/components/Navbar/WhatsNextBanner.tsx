d32593f1d2f8967da3737d559ebe7e260a5326e7
Chris Battenfield <chrisbattenfield@gmail.com>
2022-01-05 11:29:17 -0800

    reword, use nucypher url in link

--- src/components/Navbar/WhatsNextBanner.tsx
+++ src/components/Navbar/WhatsNextBanner.tsx
@@ -11,9 +11,13 @@ const WhatsNextBanner: FC<{ onClose: () => void }> = ({ onClose }) => {
       justifyContent="center"
       position="relative"
     >
-      Visit the temporary upgrade UI to upgrade your NU and KEEP to T
-      <Link ml="5px" textDecoration="underline" href={ExternalLink.TEMPORARY_UPGRADE_UI}>
-        Upgrade
+      Upgrade your NU & KEEP to T on the upgrade UI
+      <Link 
+        ml="5px" 
+        textDecoration="underline"
+        href={ExternalLink.TEMPORARY_UPGRADE_UI}
+      >
+        nucypher.network
       </Link>
       <Icon ml="5px" as={HiOutlineExternalLink} />
       <CloseButton position="absolute" right="10px" onClick={onClose} />
