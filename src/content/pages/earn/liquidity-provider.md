c5e1f789fa0c0bd2ba4c770f5507f9b673cb65ba
George Weiler <georgejweiler@gmail.com>
2022-04-01 12:52:05 -0600

    Set up liquidity provider page with hero template

--- src/content/pages/earn/liquidity-provider.md
+++ src/content/pages/earn/liquidity-provider.md
@@ -2,4 +2,13 @@
 template: earn-page/liquidity-provider
 path: /earn/liquidity-providerr
 title: Earn Liquidity Provider Page
+lpInfo:
+  title: Liquidity Provider
+  description: Liquidity providers earn yield by depositing assets into liquidity
+    pools to provide liquidity for automated market making and ease of trading
+    in the DeFi ecosystem. Take on a role as a liquidity provider by depositing
+    into one of the many Threshold liquidity pools.
+  image: /images/liquidity.png
+  buttons: []
+  rowReverse: false
 ---
--- src/templates/earn-page/liquidity-provider/index.tsx
+++ src/templates/earn-page/liquidity-provider/index.tsx
@@ -1,11 +1,18 @@
 import { FC } from "react"
 import { graphql } from "gatsby"
 import RolePageTemplate from "../RolePageTemplate"
+import SectionTemplate from "../../home-page/SectionTemplate"
 
 const LiquidityProviderPageTemplate: FC<any> = ({ data }) => {
-  const { title } = data.markdownRemark.frontmatter
+  const { lpInfo } = data.markdownRemark.frontmatter
 
-  return <RolePageTemplate>{title}</RolePageTemplate>
+  console.log("lp info ", lpInfo)
+
+  return (
+    <RolePageTemplate>
+      <SectionTemplate {...lpInfo} />
+    </RolePageTemplate>
+  )
 }
 
 export default LiquidityProviderPageTemplate
@@ -15,7 +22,21 @@ export const query = graphql`
     markdownRemark(id: { eq: $id }) {
       id
       frontmatter {
-        title
+        lpInfo {
+          rowReverse
+          title
+          description
+          image {
+            id
+            relativePath
+            internal {
+              mediaType
+            }
+            childImageSharp {
+              gatsbyImageData(width: 200)
+            }
+          }
+        }
       }
     }
   }
