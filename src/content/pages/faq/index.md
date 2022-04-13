5e1adc9031190667068921dd3b3df25defb6e101
George Weiler <georgejweiler@gmail.com>
2022-04-13 17:55:51 -0600

    Add FAQ page template with hero

--- src/content/pages/faq/index.md
+++ src/content/pages/faq/index.md
@@ -2,4 +2,16 @@
 template: faq-page
 path: /faq
 title: FAQ page
+aboutInfo:
+  rowReverse: false
+  title: About
+  description: Threshold is the first ever on-chain merge between two existing
+    networks and communities, Keep and NuCypher. Threshold provides a suite of
+    threshold cryptography services that power user sovereignty on the
+    blockchain.
+  buttons:
+    - label: About the merge
+      url: NEED_URL
+      variant: EXTERNAL_SOLID
+  image: /images/staking-threshold.png
 ---
--- src/templates/faq-page/index.tsx
+++ src/templates/faq-page/index.tsx
@@ -1,20 +1,50 @@
 import { FC } from "react"
 import { graphql } from "gatsby"
+import SectionTemplate from "../home-page/SectionTemplate"
+import { Box } from "@chakra-ui/react"
 
 const FAQPageTemplate: FC<any> = ({ data }) => {
-  const { title } = data.markdownRemark.frontmatter
+  const { aboutInfo } = data.markdownRemark.frontmatter
 
-  return <>{title}</>
+  return (
+    <Box>
+      <SectionTemplate
+        {...aboutInfo}
+        preTitle={null}
+        columnReverse
+        bgColor="gray.900"
+      />
+    </Box>
+  )
 }
 
 export default FAQPageTemplate
 
 export const query = graphql`
-  query FAQPage($id: String!) {
+  query FaqPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       frontmatter {
-        title
+        aboutInfo {
+          rowReverse
+          title
+          description
+          image {
+            id
+            relativePath
+            internal {
+              mediaType
+            }
+            childImageSharp {
+              gatsbyImageData(width: 200)
+            }
+          }
+          buttons {
+            label
+            url
+            variant
+          }
+        }
       }
     }
   }
