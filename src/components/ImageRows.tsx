c7a730ac9600e643186a2524feb2c3d228d32d30
michalsmiarowski <michal.smiarowski@thesis.co>
2023-04-13 16:26:00 +0200

    Add horizontal line to ImageRows

--- src/components/ImageRows.tsx
+++ src/components/ImageRows.tsx
@@ -17,42 +17,44 @@ interface ImageRowsProps {
 }
 
 export const ImageRows: FC<ImageRowsProps> = ({ data }) => {
-  // TODO: Add horizontal line after each row (except last one)
   return (
     <>
       {data.map((item, index, array) => {
         return (
-          <Stack direction={{ base: "column", xl: "row" }}>
-            <Stack
-              direction={{ base: "column", xl: "row" }}
-              alignItems={{ base: "flex-start", xl: "center" }}
-            >
-              <LabelMd
-                width={{ base: "500px", xl: "150px" }}
-                color={{ base: "brand.300", xl: "gray.300" }}
+          <>
+            <Stack direction={{ base: "column", xl: "row" }}>
+              <Stack
+                direction={{ base: "column", xl: "row" }}
+                alignItems={{ base: "flex-start", xl: "center" }}
               >
-                {item.title}
-              </LabelMd>
-              <Show below={"xl"}>
-                <Divider
-                  orientation="horizontal"
-                  borderTop="1px solid"
-                  color={"brand.700"}
-                />
-              </Show>
-            </Stack>
-            <Flex wrap={"wrap"}>
-              {item.images.map((imageData) => {
-                return (
-                  <Image
-                    {...imageData.image}
-                    marginInlineStart="0"
-                    px={"2px"}
+                <LabelMd
+                  width={{ base: "500px", xl: "150px" }}
+                  color={{ base: "brand.300", xl: "gray.300" }}
+                >
+                  {item.title}
+                </LabelMd>
+                <Show below={"xl"}>
+                  <Divider
+                    orientation="horizontal"
+                    borderTop="1px solid"
+                    color={"brand.700"}
                   />
-                )
-              })}
-            </Flex>
-          </Stack>
+                </Show>
+              </Stack>
+              <Flex wrap={"wrap"}>
+                {item.images.map((imageData) => {
+                  return (
+                    <Image
+                      {...imageData.image}
+                      marginInlineStart="0"
+                      px={"2px"}
+                    />
+                  )
+                })}
+              </Flex>
+            </Stack>
+            {index !== array.length - 1 && <Divider color="gray.300" />}
+          </>
         )
       })}
     </>
