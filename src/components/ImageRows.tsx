894493c712bd0c654b53f273d6cc3ca366b6b940
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-05-11 10:30:08 +0200

    Refactor `ImageRows` component
    Make the component props more generic so it can be used to render any
    type of iamges with extra data no only with `name` and `image` props.
    Here we also update the design of this component based on the Figma
    views.

--- src/components/ImageRows.tsx
+++ src/components/ImageRows.tsx
@@ -1,62 +1,48 @@
 import { FC } from "react"
+import { Divider, VStack, Box, SimpleGrid } from "@chakra-ui/react"
 import { LabelMd } from "./Typography"
 import { Image, ImageProps } from "./Image"
-import { Divider, Flex, Show, Stack } from "@chakra-ui/react"
 
-interface ImageRowData {
+interface ImageRowItem {
+  name: string
+  image: ImageProps
+}
+
+interface ImageRowData<T extends ImageRowItem> {
   title: string
-  images: {
-    name: string
-    image: ImageProps
-  }[]
+  images: T[]
 }
 
-interface ImageRowsProps {
-  data: ImageRowData[]
-  renderImageCallback?: (imageRowData: ImageRowData) => {}
+interface ImageRowsProps<T extends ImageRowItem> {
+  data: ImageRowData<T>[]
+  renderImageCallback?: (imageRowData: T) => {}
 }
 
-export const ImageRows: FC<ImageRowsProps> = ({ data }) => {
+const DefaultItem: FC<ImageRowItem> = ({ name, image }) => (
+  <Image {...image} alt={name} />
+)
+
+const renderDefaultItem = (item: ImageRowItem) => (
+  <DefaultItem key={item.name} {...item} />
+)
+
+export const ImageRows = <T extends ImageRowItem>({
+  data,
+  renderImageCallback,
+}: ImageRowsProps<T>) => {
   return (
-    <>
-      {data.map((item, index, array) => {
+    <VStack spacing="14">
+      {data.map((item) => {
         return (
-          <>
-            <Stack direction={{ base: "column", xl: "row" }}>
-              <Stack
-                direction={{ base: "column", xl: "row" }}
-                alignItems={{ base: "flex-start", xl: "center" }}
-              >
-                <LabelMd
-                  width={{ base: "500px", xl: "150px" }}
-                  color={{ base: "brand.300", xl: "gray.300" }}
-                >
-                  {item.title}
-                </LabelMd>
-                <Show below={"xl"}>
-                  <Divider
-                    orientation="horizontal"
-                    borderTop="1px solid"
-                    color={"brand.700"}
-                  />
-                </Show>
-              </Stack>
-              <Flex wrap={"wrap"}>
-                {item.images.map((imageData) => {
-                  return (
-                    <Image
-                      {...imageData.image}
-                      marginInlineStart="0"
-                      px={"2px"}
-                    />
-                  )
-                })}
-              </Flex>
-            </Stack>
-            {index !== array.length - 1 && <Divider color="gray.300" />}
-          </>
+          <Box key={item.title}>
+            <LabelMd color="brand.300">{item.title}</LabelMd>
+            <Divider borderColor="brand.700" mb="10" />
+            <SimpleGrid columns={4} spacing="14">
+              {item.images.map(renderImageCallback ?? renderDefaultItem)}
+            </SimpleGrid>
+          </Box>
         )
       })}
-    </>
+    </VStack>
   )
 }
