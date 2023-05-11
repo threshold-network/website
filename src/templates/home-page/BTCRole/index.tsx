2abcc35a219153f51626db59fa30fc9fc11e5d9f
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-05-11 10:32:50 +0200

    Render tBTC parnters on main and tBTC page

--- src/templates/home-page/BTCRole/index.tsx
+++ src/templates/home-page/BTCRole/index.tsx
@@ -1,22 +1,29 @@
 import { FC } from "react"
+import { Divider, SimpleGrid } from "@chakra-ui/react"
 import SectionTemplate, { RoleTemplateProps } from "../SectionTemplate"
-import { TBTCPartnerImage, TBTCPartners } from "./tBTCPartners"
+import { renderPartners, TBTCPartner } from "../../../components/tBTCPartners"
+import { H4 } from "../../../components"
 
 interface BTCRoleProps {
-  minters: TBTCPartnerImage[]
-  guardians: TBTCPartnerImage[]
+  partners: TBTCPartner[]
 }
 
 export const BTCRole: FC<RoleTemplateProps & BTCRoleProps> = ({
-  minters,
-  guardians,
+  partners,
   ...props
-}) => {
-  return (
-    <>
-      <SectionTemplate {...props}>
-        <TBTCPartners minters={minters} guardians={guardians} mt={"6rem"} />
-      </SectionTemplate>
-    </>
-  )
-}
+}) => (
+  <>
+    <SectionTemplate {...props}>
+      <Divider my="5.5rem" />
+      <H4 color="gray.300">tBTC is backed by the following partners.</H4>
+      <SimpleGrid
+        mt="4.5rem"
+        columns={{ base: 2, md: 4, lg: 6 }}
+        spacingY="5.5rem"
+        justifyItems="center"
+      >
+        {partners.map(renderPartners)}
+      </SimpleGrid>
+    </SectionTemplate>
+  </>
+)
