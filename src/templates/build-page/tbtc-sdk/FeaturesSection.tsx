f0df2bb9fa34e2e9b15eec0799bc6e61eacd5487
evandrosaturnino <evandro.saturnino@gmail.com>
2023-12-22 01:11:02 -0300

    Remove unused type in sdk feature section

--- src/templates/build-page/tbtc-sdk/FeaturesSection.tsx
+++ src/templates/build-page/tbtc-sdk/FeaturesSection.tsx
@@ -8,39 +8,35 @@ interface FeaturesSectionProps {
   icon: ImageProps
 }
 
-interface FeatureCardProps {
+export interface FeatureCardProps {
   title: string
   description: string
-  isBigSize?: boolean
 }
 
-const FeatureCard: FC<FeatureCardProps & FeaturesSectionProps> = ({
-  icon,
-  title,
-  description,
-}) => {
-  return (
-    <Card
-      boxShadow={"0px 4px 90px 0px rgba(153, 116, 255, 0.06)"}
-      m="auto"
-      display="flex"
-      flexDirection="column"
-      w="100%"
-      p={10}
-    >
-      <Stack spacing={6}>
-        <Stack direction="row" alignItems="center" gap={2}>
-          <Image h="10px" w="10px" {...icon} />
-          <H6>{title}</H6>
+const FeatureCard: FC<FeatureCardProps & Pick<FeaturesSectionProps, "icon">> =
+  ({ icon, title, description }) => {
+    return (
+      <Card
+        boxShadow={"0px 4px 90px 0px rgba(153, 116, 255, 0.06)"}
+        m="auto"
+        display="flex"
+        flexDirection="column"
+        w="100%"
+        p={10}
+      >
+        <Stack spacing={6}>
+          <Stack direction="row" alignItems="center" gap={2}>
+            <Image h="10px" w="10px" {...icon} />
+            <H6>{title}</H6>
+          </Stack>
+
+          <BodyMd as="div" lineHeight="28px" color="gray.400" noOfLines={3}>
+            {description}
+          </BodyMd>
         </Stack>
-
-        <BodyMd as="div" lineHeight="28px" color="gray.400" noOfLines={3}>
-          {description}
-        </BodyMd>
-      </Stack>
-    </Card>
-  )
-}
+      </Card>
+    )
+  }
 
 const FeaturesSection: FC<FeaturesSectionProps> = ({
   cards,
