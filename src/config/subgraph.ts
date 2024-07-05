5589762d9177a070b5594c3d9184242a3685884f
michalsmiarowski <michal.smiarowski@thesis.co>
2024-07-05 18:22:34 +0200

    Add REAC_APP prefix

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -4,4 +4,4 @@ export const T_NETWORK_SUBGRAPH_URL =
 export const T_NETWORK_SUBGRAPH_ID =
   "5TJAMbsRwm1avUTV4CofaLT4apfQoAiNcysEit9BWr6R"
 
-export const TBTC_SUBGRAPH_URL = `https://gateway-arbitrum.network.thegraph.com/api/${process.env.SUBGRAPH_API_KEY}/subgraphs/id/DETCX5Xm6tJfctRcZAxhQB9q3aK8P4BXLbujHmzEBXYV`
+export const TBTC_SUBGRAPH_URL = `https://gateway-arbitrum.network.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/DETCX5Xm6tJfctRcZAxhQB9q3aK8P4BXLbujHmzEBXYV`
